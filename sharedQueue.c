c1ce172bb291f189d6971df57c3bf3d4b6e5fa48
Ivo Ebbers <ijw.ebbers@outlook.com>
2019-08-25 20:14:16 +0200

    created producer threads

--- sharedQueue.c
+++ sharedQueue.c
@@ -1,62 +1,30 @@
-/******************************************************************************
- * File:         main.c
- * Version:      1.2
- * Date:         2017-10-18
- * Author:       J. Onokiewicz, M. van der Sluys
- * Description:  OPS exercise 5: Queues
- ******************************************************************************/
-
-
 #include "Queue.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 
-int main() {
-  data_t data = {1, "Hello queue"};
-  queue_t queue = {NULL};  // Note: element of queue = NULL
-  
-  printf("\nList the contents of the current queue:\n");
-  showQueue(&queue);
-  
-  printf("\nCreate a new queue:\n");
-  createQueue(&queue, data);
-  showQueue(&queue);
-  data.intVal++;
-  
-  
-  
-  printf("\nAdd new data to the queue:\n");
-  pushQueue(&queue, data);
-  showQueue(&queue);
-  data.intVal++;
-  
-  printf("\nAdd new data to the queue:\n");
-  pushQueue(&queue, data);
-  showQueue(&queue);
-  data.intVal++;
-  
-  printf("\nAdd new data to the queue:\n");
-  pushQueue(&queue, data);
-  showQueue(&queue);
-  
-  printf("\nRemove data from the queue:\n");
-  popQueue(&queue);
-  showQueue(&queue);
-  
-  
-  
-  printf("\nFront iValue/first node: %d\n", frontQueue(&queue)->intVal);
-  printf("Back  iValue/last node:  %d\n", backQueue(&queue)->intVal);
-  
-  
-  printf("\nDelete the current queue:\n");
-  deleteQueue(&queue);
-  showQueue(&queue);
-  
-  printf("\nCreate a new queue:\n");
-  createQueue(&queue, data);
-  showQueue(&queue);
-  data.intVal++;
-  deleteQueue(&queue);
+data_t thread1 = {2, "Thread 1"};
+data_t thread2 = {3, "Thread 2"};
+data_t thread3 = {4, "Thread 3"};
+
+queue_t queue = {NULL};
+
+void *producer(void *input);
+
+int main()
+{
+  pthread_t tid;
+  pthread_create(&tid, NULL, &producer, (void *)&thread1);
+  pthread_create(&tid, NULL, &producer, (void *)&thread2);
+  pthread_create(&tid, NULL, &producer, (void *)&thread3);
   return 0;
 }
+
+void *producer(void *input)
+{
+  data_t *arguments = input;
+  sleep(arguments->intVal);
+  pushQueue(&queue, *arguments);
+}
+
